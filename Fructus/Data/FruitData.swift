f2feefc3806fd1784f1a52457768fab58df5ba7f
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-11 17:35:12 +0300

    Data

---
+++ Fructus/Data/FruitData.swift
@@ -0,0 +1,19 @@
+//
+//  FruitData.swift
+//  Fructus
+//
+//  Created by Ege Aydemir on 11.10.2022.
+//
+
+import SwiftUI
+
+// MARK: Fruits Data
+
+let fruitData: [Fruit] = [
+    Fruit(title: "Blueberry",
+          headline: "Blueberries are sweet, nutritious and wildly popular fruit all over the world.",
+          image: "blueberry",
+          gradientColor: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")],
+          description: "Blueberries are perennial flowering plants with blue or purple berries.",
+          nutrition: ["240 kJ","9.96 g", "0.33 g", "0.74 g", "A, B1", "Calcium, Magnasium"])
+]
