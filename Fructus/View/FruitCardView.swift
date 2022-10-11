a3b5955f30a1dd3adfebeec6dd019cdd5e5b022f
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-11 15:01:12 +0300

    Background

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -12,17 +12,22 @@ struct FruitCardView: View {
     
     // MARK: Body
     var body: some View {
-        VStack(spacing: 20) {
-            // FRUIT: IMAGE
-            Image("blueberry")
-                .resizable()
-                .scaledToFit()
-                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.90), radius: 8, x: 6, y: 8)
-            // FRUIT: TITLE
-            Text("Blueberry")
-            // FRUIT: HEADLINE
-            // BUTTON: START
-        } //: VStack
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.90), radius: 8, x: 6, y: 8)
+                // FRUIT: TITLE
+                Text("Blueberry")
+                // FRUIT: HEADLINE
+                // BUTTON: START
+            }//: VStack
+        } //: ZStack
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(.linearGradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 // MARK: Preview
