7f3514e35700062c45730209b621374eca5c5519
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-11 14:47:33 +0300

    FRUIT IMAGE

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -14,6 +14,10 @@ struct FruitCardView: View {
     var body: some View {
         VStack(spacing: 20) {
             // FRUIT: IMAGE
+            Image("blueberry")
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.90), radius: 8, x: 6, y: 8)
             // FRUIT: TITLE
             Text("Blueberry")
             // FRUIT: HEADLINE
