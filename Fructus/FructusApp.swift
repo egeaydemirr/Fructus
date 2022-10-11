96f326031f52c0aa03eeb5b0550d6b32ebd26637
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-11 12:32:45 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ege Aydemir on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
