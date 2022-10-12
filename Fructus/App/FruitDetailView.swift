35a6db5eeb7e2708bfb4fc7fcdae7cfd04e2ef71
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-12 23:22:11 +0300

    Fruit Detail View Create

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Ege Aydemir on 12.10.2022.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView()
+    }
+}
