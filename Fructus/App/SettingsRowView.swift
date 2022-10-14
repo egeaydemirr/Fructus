f1d7f07a6fa4ea65a803f32893574d2a4b042d6b
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-14 16:49:55 +0300

    Create Settings Row

--- Fructus/App/SettingsRowView.swift
+++ Fructus/App/SettingsRowView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct SettingsRowView: View {
+        //MARK: PROPERTIES
+        var name: String
+        var content : String
+    
+        //MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Text(name).foregroundColor(.gray)
+            Spacer()
+            Text(content)
+        }
     }
 }
-
+//MARK: PREVIEW
 struct SettingsRowView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsRowView()
+        SettingsRowView(name:"Developer", content:"John / Jane")
+            .previewLayout(.fixed(width: 375, height: 60))
+            .padding(.vertical, 4)
     }
 }
