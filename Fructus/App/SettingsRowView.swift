19c02f19b74bd458892ddcadcdceee9e55ec3893
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-14 17:15:42 +0300

    Conditional Layout
    **

--- Fructus/App/SettingsRowView.swift
+++ Fructus/App/SettingsRowView.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct SettingsRowView: View {
         //MARK: PROPERTIES
-        var name: String
-        var content : String
+    var name: String
+    var content : String? = nil
+    var linkLabel : String? = nil
+    var linkDestination : String? = nil
+    
     
         //MARK: BODY
     var body: some View {
         HStack{
             Text(name).foregroundColor(.gray)
             Spacer()
-            Text(content)
+            if content != nil {
+                Text(content!)
+            } else if (linkLabel != nil && linkDestination != nil) {
+                Link (linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
+                Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+            } else {
+                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+            }
         }
     }
 }
